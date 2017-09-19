f13a00d44dfbad82a0bcd109eb484b5c74bf61fc
jlimm <jlimm@gmx.de>
2017-09-19 16:59:59 +0200

    test github

--- git test/git_test.iOS/Main.cs
+++ git test/git_test.iOS/Main.cs
@@ -15,6 +15,7 @@ namespace git_test.iOS
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
+            // hallo lu ping
         }
     }
 }
